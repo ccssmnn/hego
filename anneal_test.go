31cc4fdcef8b616a15437170832c9d761a4a44af
Carl <ccssmnn+github@gmail.com>
2021-01-07 07:41:17 +0000

    100% for anneal, setting verbose and testing verify

--- anneal_test.go
+++ anneal_test.go
@@ -15,17 +15,46 @@ func (b state) Neighbor() AnnealingState {
 	return b
 }
 
+func TestVerify(t *testing.T) {
+	settings := SASettings{}
+	settings.Temperature = 100.0
+	settings.AnnealingFactor = 0.9
+	err := settings.Verify()
+	if err != nil {
+		t.Error("verification should pass for temperature above 0 and annealing factor in (0,1]")
+	}
+	settings.Temperature = 0.0
+	err = settings.Verify()
+	if err == nil {
+		t.Error("verification should fail for temperature = 0.0")
+	}
+	settings.Temperature = 100.0
+	settings.AnnealingFactor = 1.1
+	err = settings.Verify()
+	if err == nil {
+		t.Error("verification should fail for annealing factor above 1")
+	}
+	settings.AnnealingFactor = 0.0
+	err = settings.Verify()
+	if err == nil {
+		t.Error("verification should fail for annealing factor <= 0")
+	}
+}
+
 // TestAnnealBit runs the AnnealBit method and checks for errors
 func TestSA(t *testing.T) {
 	initialState := state{false, true, false}
 
 	settings := SASettings{}
+	res, err := SA(initialState, settings)
+	if err == nil {
+		t.Error("SA should fail with invalid settings")
+	}
 	settings.Temperature = 100.0
 	settings.AnnealingFactor = 0.9
 	settings.MaxIterations = 10
-	settings.Verbose = 0
-
-	res, err := SA(initialState, settings)
+	settings.Verbose = 1
+	res, err = SA(initialState, settings)
 	if err != nil {
 		t.Errorf("Error while running Anneal main algorithm: %v", err)
 	}
