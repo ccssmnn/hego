a16312172c7a6946287801c6239569ae8958d2a0
Carl <ccssmnn+github@gmail.com>
2021-01-07 07:17:56 +0000

    100% for crossover with panic checks

--- crossover/continuous_test.go
+++ crossover/continuous_test.go
@@ -11,4 +11,11 @@ func TestArithmetic(t *testing.T) {
 			t.Error("unexpected range in created value")
 		}
 	}
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("crossover method should panic for inputs with unequal lengths")
+		}
+	}()
+	d := []float64{1.0, 2.0, 3.0}
+	Arithmetic(a, d, [2]float64{0.0, 1.0})
 }
